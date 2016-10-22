8c6d6a70fc3d53edd27ab2b3112de10398866661
Ben Paddock <pads@thisispads.me.uk>
2016-10-22 16:36:45 +0100

    Convert base64 image to png for paperclip

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,10 +1,28 @@
 class Entry < ApplicationRecord
+  attr_accessor :content_type, :original_filename, :image_data
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
   before_save :normalize_twitter_handle
+  before_save :decode_base64_image
 
   def normalize_twitter_handle
     self.twitter = self.twitter.gsub('@', '')
+  end
+
+  def decode_base64_image
+    if image_data && content_type && original_filename
+      decoded_data = Base64.decode64(image_data)
+
+      data = StringIO.new(decoded_data)
+      data.class_eval do
+        attr_accessor :content_type, :original_filename
+      end
+
+      data.content_type = content_type
+      data.original_filename = File.basename(original_filename)
+
+      self.image = data
+    end
   end
 end
