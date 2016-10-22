ed9228809ff9ce09138399355d88959cf5ceb62a
Ben Paddock <pads@thisispads.me.uk>
2016-10-22 15:41:01 +0100

    Add photo capture with post submission

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,72 @@
 //= require jquery_ujs
 
 //= require_tree .
+
+$(document).ready(function() {
+  if (Modernizr.getusermedia) {
+    // Configure user media
+    var gumPromise = navigator.mediaDevices.getUserMedia({
+      video: {
+        width: {
+          ideal: 320
+        },
+        height: {
+          ideal: 320
+        },
+        facingMode: 'user'
+      }
+    });
+
+    // Start streaming the video
+    gumPromise.then(function(mediaStream) {
+      var video = document.querySelector('video');
+      video.srcObject = mediaStream;
+      video.onloadedmetadata = function() {
+        video.play();
+      };
+    }).catch(function(err) {
+      alert(err);
+    });
+
+    // Capture the screenshot
+    $('#capture-btn').click(function() {
+      var video = document.getElementById('video');
+      var canvas = document.getElementById('canvas');
+      var photo = document.getElementById('photo');
+
+      var context = canvas.getContext('2d');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+      var data = canvas.toDataURL('image/png');
+
+      photo.setAttribute('src', data);
+      $('#video').hide();
+      $('#capture-btn').hide();
+      $('#photo').show();
+      $('#form').show();
+    });
+
+    // Submit the screenshot
+    $('#form').submit(function(event) {
+      var photo = document.getElementById('photo');
+      var payload = {
+        entry: {
+          twitter: $('#twtter-handle').val(),
+          photo: photo.src
+        }
+      };
+      $.ajax({
+        method: 'POST',
+        url: '/entries',
+        contentType: 'application/json',
+        data: JSON.stringify(payload)
+      });
+      event.stopImmediatePropagation();
+      event.preventDefault();
+    });
+
+  } else {
+    alert('Sorry, you need modern web browser to play this game. I hear edge is OK');
+  }
+});
