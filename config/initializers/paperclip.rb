13522518de34b1241fa59907b5482ac3658dd1b4
Daniel Hollands <daniel@limeblast.co.uk>
2016-10-22 16:02:01 +0100

    trying again

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,1 @@
-Paperclip::Attachment.default_options[:url]  = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 'in-your-face-production.s3-website-eu-west-1.amazonaws.com'
